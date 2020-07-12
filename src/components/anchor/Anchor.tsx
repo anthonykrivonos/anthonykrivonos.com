b9921183a11ac7ff4d0c9f32a82000456093a339
Anthony Krivonos <abkrivonos@gmail.com>
2020-07-12 09:05:38 -0400

    Fixed anchor bug

--- src/components/anchor/Anchor.tsx
+++ src/components/anchor/Anchor.tsx
@@ -22,12 +22,26 @@ export class Anchor extends Component<AnchorProps> {
         const { children, className, underlineColor, href, onClick } = this.props
         const { underlined } = this.state
         return (
-            <View onClick={onClick} className={'anchor-container position-relative clickable'} onMouseOver={this.onHover} onMouseOut={this.onHoverOut}>
+            <View onClick={onClick} className={'anchor-container position-relative clickable d-flex flex-row'} onMouseOver={this.onHover} onMouseOut={this.onHoverOut}>
                 {
-                    children.split('').map((char, i) => (
-                        <View key={`${children}-${char}-${i}`} className={'position-relative'} inline>
-                            <View inline className={addClasses(className || '', 'm-0 p-0')}>{char}</View>
-                            { underlined && <View inline style={{ backgroundColor: underlineColor }} className={'anchor-underline w-100'}></View> }
+                    children.split(' ').map((word, i) => (
+                        <View>
+                            {
+                                word.split('').map((char, j) => (
+                                    <View key={`inner-${char}-${j}`} className={'position-relative'} inline>
+                                        <View inline className={addClasses(className || '', 'm-0 p-0')}>{char}</View>
+                                        { underlined && <View style={{ backgroundColor: underlineColor }} className={'anchor-underline w-100'}></View> }
+                                    </View>
+                                ))
+                            }
+                            {
+                                i !== children.length - 1 &&
+                                <View inline>
+                                    <View key={`inner-space-${i}`} className={'position-relative'} inline>
+                                        <View inline className={addClasses(className || '', 'm-0 p-0')}>&nbsp;</View>
+                                    </View>
+                                </View>
+                            }
                         </View>
                     ))
                 }
