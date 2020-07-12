9a82832b6853451fb831ec0fe1dc6778e7febbd8
Anthony Krivonos <abkrivonos@gmail.com>
2020-07-12 13:13:30 -0400

    Trying to fix CSS MIME type issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,12 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeLinkHeaders: false,
+        mergeCachingHeaders: false
+      },
+    }
   ],
 }
